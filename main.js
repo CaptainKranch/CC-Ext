c1180df3fb95cd77f7bf44e46fbf2ca905017fc6
Daniel Garcia Medina <33882846+CaptainKranch@users.noreply.github.com>
2021-08-03 21:48:34 -0500

    uwu

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
 let links = [];
-const dictonary = {Confluence:0, Jira:1, Tools:2, Drive:3}
+let names = [];
+
 const inputName = document.getElementById('input-name')
 const inputLink = document.getElementById('input-link')
+const newJira = document.getElementById('jira-links')
+const newOther = document.getElementById('otros-links')
 const saveBtn = document.getElementById('save-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const linkssFromLocalStorage = JSON.parse(localStorage.getItem('links'))
-function render(links) {
+
+function render(links, names) {
   let listItems = ''
-  for (let i = 0; i < links.length; i++) {
-    listItems += `
-      <li>
-        <a target="_blank" href = "${links[i]}">
-          ${links[i]}
-        </a>
-      </li>
-    `
+  const dictonary = {
+    Jira: 0,
+    Others: 1
   }
-  ulEl.innerHTML = listItems //debo de poner un condicional que verifique en el dicc
+  for (let x = 0; x < names.length; x++) {
+    if (names in dictonary.keys[0]) {
+      for (let i = 0; i < links.length; i++) {
+        listItems += `
+          <li>
+            <a target="_blank" href = "${links[i]}">
+              ${links[i]}
+            </a>
+          </li>
+        `
+        newJira.innerHTML = listItems //debo de poner un condicional que verifique en el dicc
+      }
+    } else {
+      for (let i = 0; i < links.length; i++) {
+        listItems += `
+            <li>
+              <a target="_blank" href = "${links[i]}">
+                ${links[i]}
+              </a>
+            </li>
+          `
+        newOther.innerHTML = listItems
+      }
+    }
+  }
 }
 
-if (leadsFromLocalStorage) {
-    links = leadsFromLocalStorage
-    render(links)
-}
+saveBtn.addEventListener("click", function() {
+  links.push(inputLink.value)
+  inputLink.value = ''
+  localStorage.setItem('links', JSON.stringify(links))
+  names.push(inputName.value)
+  names.value = ''
+  localStorage.setItem('names', JSON.stringify(names))
+  render(links, names)
+})
---
+++ testing.js
@@ -0,0 +1,17 @@
+uwu = []
+usu = {}
+
+function diccmanagment (uwu) {
+  let emptyStr = ''
+  const dictonary = {Confluence:0, Jira:1, Tools:2, Drive:3}
+  if
+  for (let i = 0; i < uwu.length; i++){
+    emptyStr += `
+      <li>
+        <a target="_blank" href="${uwu[i]}">
+          ${uwu[i]}
+        </a>
+      </li>
+    `
+  }
+}
