8283bd522ff930c8fde846292045ae49aa3f3098
Daniel Garcia Medina <33882846+CaptainKranch@users.noreply.github.com>
2021-08-04 16:28:13 -0500

    uwu

--- main.js
+++ main.js
@@ -1,57 +1,48 @@
 let links = [];
 let names = [];
 
-const inputName = document.getElementById('input-name')
+const inputTag = document.getElementById('input-name')
 const inputLink = document.getElementById('input-link')
-const newJira = document.getElementById('jira-links')
-const newOther = document.getElementById('otros-links')
+const newLink = document.getElementById('link-list')
 const saveBtn = document.getElementById('save-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const linksFromLocalStorage = JSON.parse(localStorage.getItem('links'))
 const namFromLocalStorage = JSON.parse(localStorage.getItem('names'))
 
+
 function render(links, names) {
   let listItems = ''
-  const verification = ['Jira','jira']
-  if (verification.includes(names.slice(-1)[0])) {
-    for (let i = 0; i < links.length; i++) {
-      listItems += `
+  for (let i = 0; i < links.length; i++) {
+    listItems += `
           <li>
             <a target="_blank" href = "${links[i]}">
-              ${links[i]}
+              ${names[i]}
             </a>
           </li>
         `
-    }
-    newJira.innerHTML = listItems //debo de poner un condicional que verifique en el dicc
-  } else {
-    for (let i = 0; i < links.length; i++) {
-      listItems += `
-            <li>
-              <a target="_blank" href = "${links[i]}">
-                ${links[i]}
-              </a>
-            </li>
-          `
-    }
-    newOther.innerHTML = listItems
   }
+  newLink.innerHTML = listItems
 }
 
 if (linksFromLocalStorage) {
-    links = linksFromLocalStorage
-    render(links, names)
+  links = linksFromLocalStorage
+  render(links)
+}
+
+if (namFromLocalStorage) {
+  names = namFromLocalStorage
+  render(names)
 }
 
 saveBtn.addEventListener('click', function() {
   links.push(inputLink.value)
   inputLink.value = ''
-  names.push(inputName.value)
+  names.push(inputTag.value)
   names.value = ''
   render(links, names)
 })
 
-deleteBtn.addEventListener('click', function() {
+deleteBtn.addEventListener('dblclick', function() {
   localStorage.clear()
   links = []
   names = []
